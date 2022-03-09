60d58e49334cd0c18dc1a73dfa1ce452e053555c
Md Sajib Khan <mahfujsajib1998@gmail.com>
2022-03-10 01:53:34 +0600

    footer add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
 import resume from "../../assets/mdsajibkhan-resume.pdf";
-import meimg from '../../assets/myimg.jpg';
-import {BsLinkedin} from 'react-icons/bs';
-import {BsGithub} from 'react-icons/bs';
+import meimg from "../../assets/myimg.jpg";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 // import {SiNetlify} from 'react-icons/si';
 import "./Header.css";
 const Header = () => {
@@ -10,7 +10,7 @@ const Header = () => {
     <header>
       <div className="container">
         <h4>Hello, I'm</h4>
-        <h2 style={{color:"#4db5ff"}}>Mahfuj Khan Sajib</h2>
+        <h2 style={{ color: "#4db5ff" }}>Mahfuj Khan Sajib</h2>
         <h5 className="text-light">MERN Stack Developer</h5>
         <div className="download-btn">
           <button className="downloadresumebtn">
@@ -19,24 +19,38 @@ const Header = () => {
             </a>
           </button>
           <button className="downloadresumebtn">
-            <a className="downloadresume" href="#contact">Let's talk</a>
+            <a className="downloadresume" href="#contact">
+              Let's talk
+            </a>
           </button>
         </div>
         <div className="socialicons">
-          <a className="icons" href="https://github.com/MahfujSajib" target="_blank "><BsLinkedin></BsLinkedin></a>
-          <a className="icons" href="https://github.com/MahfujSajib" target="_blank"><BsGithub></BsGithub></a>
+          <a
+            className="icons"
+            href="https://www.linkedin.com/in/mahfuj-khan-sajib/"
+            target="_blank "
+          >
+            <BsLinkedin></BsLinkedin>
+          </a>
+          <a
+            className="icons"
+            href="https://github.com/MahfujSajib"
+            target="_blank"
+          >
+            <BsGithub></BsGithub>
+          </a>
           {/* <a className="icons" href="https://github.com/MahfujSajib" target="_blank"><SiNetlify></SiNetlify></a> */}
         </div>
         <div className="me">
-          <img className="meimg" src={meimg} alt='me' />
+          <img className="meimg" src={meimg} alt="me" />
         </div>
 
-        <a href="#contact" className="scroll-down">Scroll Down</a>
-        
+        <a href="#contact" className="scroll-down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
 };
 
 export default Header;
-
